34dbb0d4d77d696a354813ca1f62c40b137685f9
kunlp885 <Kunlapath.Paengsa@innopiaglobal.com>
2023-10-11 23:28:32 +0700

    Add project files.

---
+++ PrimeBaseCalc/MathBenchmark.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+public class MathBenchmark
+{
+    private readonly double doubleValue = 3.14159265358979323846;
+    private readonly decimal decimalValue = 3.14159265358979323846M;
+    readonly double doublePi = Math.PI;
+
+
+    [Benchmark]
+    public double DoubleComputation() => doubleValue * doubleValue;
+
+    [Benchmark]
+    public decimal DecimalComputation() => decimalValue * decimalValue;
+
+    [Benchmark]
+    public double DoublePiComputation() => doublePi * doublePi;
+
+    [Benchmark]
+    public decimal DecimalPiComputation() => (decimal)doublePi * (decimal)doublePi;
+}
---
+++ PrimeBaseCalc/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        decimal a = (decimal)Math.PI;
+        decimal b = (decimal)Math.E;
+        decimal ab57 = a * b * 5 * 7*9;
+        sw.Stop();
+        Console.WriteLine(ab57);
+        Console.WriteLine(sw.ElapsedTicks);
+
+        //sw.Reset();
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        double c = Math.PI;
+        double d = Math.E;
+        double cd57 = c * d * 5 * 7*9;
+        stopwatch.Stop();
+        Console.WriteLine(cd57);
+        Console.WriteLine(stopwatch.ElapsedTicks);
+    }
+}
+
+/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //var summary = BenchmarkRunner.Run<MathBenchmark>();
+        //Console.WriteLine(summary);
+        int limit = 100; // Change the limit as needed
+        IEnumerable<int> primeSequence = PrimeCompareInt.GetPrimes(limit);
+        IEnumerable<int> integerSequence = PrimeCompareInt.GetIntegers(limit);
+        var commonNumbers = primeSequence.Intersect(integerSequence);
+
+        Console.WriteLine("Prime Numbers: " + string.Join(", ", primeSequence));
+        Console.WriteLine("Integer Numbers: " + string.Join(", ", integerSequence));
+
+        Console.WriteLine("Common Numbers: " + string.Join(", ", commonNumbers));
+
+    }
+}*/
+
+public class PrimeCompareInt
+{
+    public static IEnumerable<int> GetPrimes(int n)
+    {
+        if (n < 2) yield break;
+
+        List<int> primes = new List<int> { 2 };
+        yield return 2;
+
+        for (int i = 3; i <= n; i += 2)
+        {
+            if (primes.All(p => i % p != 0))
+            {
+                primes.Add(i);
+                yield return i;
+            }
+        }
+    }
+
+    public static IEnumerable<int> GetIntegers(int n)
+    {
+        return Enumerable.Range(1, n);
+    }
+}
