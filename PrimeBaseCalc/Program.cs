2d047d0ac4c0c663c50122dbf3ce7d35ffb77238
kunlp885 <Kunlapath.Paengsa@innopiaglobal.com>
2023-10-12 00:35:46 +0700

    generic

--- PrimeBaseCalc/Program.cs
+++ PrimeBaseCalc/Program.cs
@@ -5,33 +5,37 @@
         //var summary = BenchmarkRunner.Run<MathBenchmark>();
         //Console.WriteLine(summary);
         int limit = 100; // Change the limit as needed
-        IEnumerable<int> primeSequence = PrimeCompareInt.GetPrimes(limit);
-        IEnumerable<int> integerSequence = PrimeCompareInt.GetIntegers(limit);
-        var commonNumbers = primeSequence.Intersect(integerSequence);
+        //IEnumerable<int> primeSequence = PrimeCompareInt.GetPrimes(limit);
+        //IEnumerable<int> integerSequence = PrimeCompareInt.GetIntegers(limit);
+        //var commonNumbers = primeSequence.Intersect(integerSequence);
 
-        Console.WriteLine("Prime Numbers: " + string.Join(", ", primeSequence));
-        Console.WriteLine("Integer Numbers: " + string.Join(", ", integerSequence));
+        //Console.WriteLine("Prime Numbers: " + string.Join(", ", primeSequence));
+        //Console.WriteLine("Integer Numbers: " + string.Join(", ", integerSequence));
 
-        Console.WriteLine("Common Numbers: " + string.Join(", ", commonNumbers));
+        //Console.WriteLine("Common Numbers: " + string.Join(", ", commonNumbers));
+        var t1 = new CustomNumber<string>("max");
+        //var t2 = new CustomNumber<int>(10);
+        var t2 = new CustomNumber<decimal>(9.87m);
+        Console.WriteLine(t1 <= t2);
 
     }
 }
 
-public class CustomNumber
+public class CustomNumber<T>(T generic)
 {
-    public decimal Decimal { get; }
+    public T Value { get; } = generic;
 
-    public CustomNumber(decimal value)
+    public static string operator <=(CustomNumber<T> a, CustomNumber<T> b)
     {
-        Decimal = value;
+        return $"<p>Hello, world.{a.Value} and {b.Value}</p>";
     }
 
-    public static CustomNumber operator +(CustomNumber a, CustomNumber b)
+    public static string operator <=(CustomNumber<string> a, CustomNumber<T> b)
     {
-        return new CustomNumber(a.Decimal + b.Decimal);
+        return $"<p>Hello, world.{a.Value} and {b.Value}</p>";
     }
-    public override string ToString()
-    {
-        return Decimal.ToString();
-    }
+
+    //
+    public static string operator >=(CustomNumber<string> a, CustomNumber<T> b) => "";
+    public static string operator >=(CustomNumber<T> a, CustomNumber<T> b) => "";
 }
