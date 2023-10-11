525046fc8dbe3862f699cd30602d72bf9562bd26
kunlp885 <Kunlapath.Paengsa@innopiaglobal.com>
2023-10-11 23:29:17 +0700

    primeSequence

--- PrimeBaseCalc/Program.cs
+++ PrimeBaseCalc/Program.cs
@@ -1,32 +1,4 @@
-﻿using System;
-using BenchmarkDotNet.Running;
-using System;
-using System.Diagnostics;
-
-class Program
-{
-    static void Main()
-    {
-        Stopwatch sw = Stopwatch.StartNew();
-        decimal a = (decimal)Math.PI;
-        decimal b = (decimal)Math.E;
-        decimal ab57 = a * b * 5 * 7*9;
-        sw.Stop();
-        Console.WriteLine(ab57);
-        Console.WriteLine(sw.ElapsedTicks);
-
-        //sw.Reset();
-        Stopwatch stopwatch = Stopwatch.StartNew();
-        double c = Math.PI;
-        double d = Math.E;
-        double cd57 = c * d * 5 * 7*9;
-        stopwatch.Stop();
-        Console.WriteLine(cd57);
-        Console.WriteLine(stopwatch.ElapsedTicks);
-    }
-}
-
-/*internal class Program
+﻿class Program
 {
     private static void Main(string[] args)
     {
@@ -43,7 +15,7 @@ class Program
         Console.WriteLine("Common Numbers: " + string.Join(", ", commonNumbers));
 
     }
-}*/
+}
 
 public class PrimeCompareInt
 {
